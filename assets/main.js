6d61fa1e061ff037b5444e28e9b001b478a79c6c
bineko204 <trinhkhanh.204@gmail.com>
2020-06-04 20:49:34 +0700

    update cv

---
+++ assets/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    var $element = $('.element');
+    if ($element.length) {
+        var options = {
+            strings: $element.attr('data-elements').split(','),
+            typeSpeed: 100,
+            backDelay: 3000,
+            backSpeed: 50,
+            loop: true,
+        };
+        var typed = new Typed('.element', options);
+    }
+
+    $('#pagepiling').pagepiling({
+        menu: null,
+        direction: 'vertical',
+        verticalCentered: true,
+        sectionsColor: [],
+        anchors: ['home', 'about', 'resume', 'project'],
+        scrollingSpeed: 700,
+        easing: 'linear',
+        loopBottom: true,
+        loopTop: true,
+        css3: true,
+        navigation: {
+            textColor: '#000',
+            bulletsColor: '#000',
+            position: 'right',
+            tooltips: ['Trang chủ', 'Giới thiệu', 'Hồ sơ', 'Dự án'],
+        },
+        normalScrollElements: null,
+        normalScrollElementTouchThreshold: 5,
+        touchSensitivity: 5,
+        keyboardScrolling: true,
+        sectionSelector: '.section',
+        animateAnchor: true,
+
+        //events
+        onLeave: function (index, nextIndex, direction) {},
+        afterLoad: function (anchorLink, index) {},
+        afterRender: function () {},
+    });
+
+    $('.menu-icon, .close-menu, .menu-item').click(function () {
+        $('.menu').hasClass('show') ? $('.menu').removeClass('show') : $('.menu').addClass('show');
+    });
+});
